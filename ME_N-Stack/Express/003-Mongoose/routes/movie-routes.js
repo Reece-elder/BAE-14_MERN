c2bd0e592be4a0628a3b2f26ea32b7ac21d9af52
reece-elder <reece.elder@qa.com>
2022-05-23 16:05:06 +0100

    CRUD Functions

--- ME_N-Stack/Express/003-Mongoose/routes/movie-routes.js
+++ ME_N-Stack/Express/003-Mongoose/routes/movie-routes.js
@@ -26,9 +26,69 @@ router.post("/create", (req, res, next) => {
         });
 });
 
-router.post("/createTwo", (req, res, next) => {
-    res.send("Does this work now?");
+router.post("/createMany", (req, res, next) => {
+
+    // Making a variable of our body, this variable will be an array
+    const data = req.body; 
+
+    // Loops through an array, for each item in an array.. do this thing
+    data.forEach((movieData) => {
+        const movieObj = new movie(movieData); // Make a new object
+        movieObj.save().then((result) => {
+            // our method can only have one response  
+            console.log(`${result.title} has been added to database`);
+        }).catch((error) => {
+            next(error); 
+        });
+    });
+    res.status(201).send("All Data added to DB");
 });
+
+// Read Request(s)
+// GetAll 
+// GetById MySQL - (1, 2, 530) mongoDB ids (bfyebf37383782b3bf2e2)
+router.get("/getId/:id", (req, res, next) => {
+    const id = req.params.id;
+    // finding the object by id
+    movie.findById(id, (error, movie) => {
+        // doing this with the object that is found (returning the data)
+        res.status(200).send(movie); 
+    });
+});
+
+router.get("/getRating/:rating", (req, res, next) => {
+    const rating = req.params.rating;
+    // finding the object by id
+    // Once it finds one data, it stops
+    movie.findOne({'rating' : rating}, (error, movie) => {
+        res.status(200).send(movie);
+    });
+});
+
+router.get("/getAll", (req, res, next) => {
+    movie.find((error, movies) => {
+        res.status(200).send(movies);
+    });
+});
+
+router.delete("/deleteId/:id", (req, res, next) => {
+    movie.findByIdAndDelete(req.params.id, (error) => {
+        res.status(202).send("movie deleted");
+    });
+});
+
+// Update takes in a request body AND a request id
+router.put("/updateId/:id", (req, res, next) => {
+    const data = req.body;
+    const id = req.params.id;
+    movie.findByIdAndUpdate(id, data, (error, movie) => {
+        res.status(202).send("Movie updated!");
+    });
+});
+
+
+// movie.deleteMany({'query' : value})
+
 
 
 
