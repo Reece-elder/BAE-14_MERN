dd769ad2d1ce1709c4aa86fb4cc9008b392220b4
reece-elder <reece.elder@qa.com>
2022-05-24 10:58:59 +0100

    001-components

--- MERN-Stack/React/bae-react-demo/src/App.js
+++ MERN-Stack/React/bae-react-demo/src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import HelloWorld from './components/001-Components/HelloWorld';
+import './css/App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.. please.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Importing our HelloWorld component into our App.js */}
+      <HelloWorld/>
+      {/* All normal HTML elements are lowercase, capital letters let us know they are Components */}
     </div>
   );
 }
